adedcc0599f54209e7f6fd72da8eb1a18e891644
wlx <31901209@stu.zucc.edu.cn>
2022-06-26 11:01:21 +0800

    云开发初始化配置

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 // app.js
 App({
+  //小程序一启动就会执行
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+    console.log('小程序开始启动拉')
+    wx.cloud.init({
+      env: 'cloud1-1gwwzozxde8dfbf2' //云开发环境id
     })
   },
-  globalData: {
-    userInfo: null
-  }
 })
