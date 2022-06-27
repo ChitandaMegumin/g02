75b033ba8c64a94cc12a5b318d04233ef63c1744
zucc-31901211 <1325060469@qq.com>
2022-06-27 15:34:27 +0800

    商品选择静态页面布局已完成
    静态信息

--- pages/goodsselect/goodsselect.js
+++ pages/goodsselect/goodsselect.js
@@ -11,8 +11,13 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
+  onLoad (options){
+      wx.cloud.database().collection('commodity').get().then(res=>{
+        console.log('请求到的数据',res)
+        this.setData({
+          list: res.data
+        })
+      })
   },
 
   /**
