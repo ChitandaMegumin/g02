ddc28e83ff84018fe9009057d541a196668d2879
wanyib <1456034259@qq.com>
2022-06-27 14:17:46 +0800

    首页 v0.2
    更新首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,17 @@
 Page({
+
   onLoad(){
-    
+    wx.cloud.database().collection("customer")
+    .doc("16db756f62b9449d08afcc12701b1417")
+    .get()
+    .then(res=>{
+      console.log("当前用户的积分获取成功",res)
+      this.setData({
+        code:res.data
+      }) 
+    })
+    .catch(err=>{
+      console.log("当前用户的积分获取失败",err)
+    })
   }
 })
