1491f8ee3d113f0d008ace24c3b627644ae3022f
wlx <31901209@stu.zucc.edu.cn>
2022-07-01 10:49:09 +0800

    获得全部订单和删除订单

---
+++ cloud/getAllorders/index.js
@@ -0,0 +1,11 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+
+// 云函数入口函数
+exports.main = async (event, context) => { //查看所有的订单
+  return cloud.database().collection('orders').get()
+}
