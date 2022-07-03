7606efc8f9ef886b869c6860ae6ab2bb1660bc7a
zucc-31901211 <1325060469@qq.com>
2022-07-03 11:38:40 +0800

    结算界面动态数据及布局基本完成

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -5,14 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    time: '12:00',
+    goodsincart:[],
+    totalnum:0
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    
+    this.countTotalPrice()
   },
 
   /**
@@ -62,5 +65,27 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+  bindTimeChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  countTotalPrice(){
+    let temp = 0 
+    let currentnum=0
+    this.setData({
+      goodsincart:wx.getStorageSync('cartList')
+    })
+    for(let idx in this.data.goodsincart){      
+      temp = temp + this.data.goodsincart[idx].Commodity_currentnum* this.data.goodsincart[idx].Commodity_price
+      currentnum = currentnum+this.data.goodsincart[idx].Commodity_currentnum
+    }
+    console.log('目前总数',temp)
+    this.setData({
+      totalnum:temp,
+    })
   }
 })
